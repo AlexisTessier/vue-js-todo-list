d19fa6fc73267413e5a5dd01629bc4f5cf2770f9
Alexis Tessier <>
2019-02-04 15:51:40 +0100

    layout progress

--- web-app/pages/index.vue
+++ web-app/pages/index.vue
@@ -1,37 +1,42 @@
 <template>
   <div>
     <v-toolbar flat>
-      <v-toolbar-title>Todos</v-toolbar-title>
-
-      <v-spacer />
-      <v-tooltip left>
-        <v-btn slot="activator" icon>
-          <v-icon>add</v-icon>
-        </v-btn>
-        <span>Create new todo</span>
-      </v-tooltip>
+      <v-toolbar-title>Todos</v-toolbar-title>      
     </v-toolbar>
     <v-content>
       <v-container>
         <v-progress-circular
           indeterminate
         />
-
+        <v-btn block light>Add new todo</v-btn>
         <v-expansion-panel>
           <v-expansion-panel-content>
-            <div slot="header">hello</div>
+            <div slot="header">
+              hello
+            </div>
             hello world
           </v-expansion-panel-content>
           <v-expansion-panel-content>
-            <div slot="header">hello</div>
+            <div slot="header">
+              hello
+            </div>
             hello world
           </v-expansion-panel-content>
           <v-expansion-panel-content>
-            <div slot="header">hello</div>
+            <div slot="header">
+              hello
+            </div>
             hello world
           </v-expansion-panel-content>
         </v-expansion-panel>
       </v-container>
+
+      <v-tooltip left>
+        <v-btn slot="activator" icon>
+          <v-icon>add</v-icon>
+        </v-btn>
+        <span>Add new todo</span>
+      </v-tooltip>
     </v-content>
   </div>
 </template>
---
+++ web-app/store/todos.js
@@ -0,0 +1,8 @@
+function createTask(){
+	return {
+		id: "42",
+		title: "a task",
+		done: false,
+		description: "description"
+	}
+}
