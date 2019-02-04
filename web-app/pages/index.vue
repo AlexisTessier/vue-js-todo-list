1b9323d606d6956c050ff4fd35b0e3762facaf6f
Alexis Tessier <>
2019-02-04 15:36:24 +0100

    start working on todos layout

--- web-app/pages/index.vue
+++ web-app/pages/index.vue
@@ -1,13 +1,46 @@
 <template>
-  <hello-world />
+  <div>
+    <v-toolbar flat>
+      <v-toolbar-title>Todos</v-toolbar-title>
+
+      <v-spacer />
+      <v-tooltip left>
+        <v-btn slot="activator" icon>
+          <v-icon>add</v-icon>
+        </v-btn>
+        <span>Create new todo</span>
+      </v-tooltip>
+    </v-toolbar>
+    <v-content>
+      <v-container>
+        <v-progress-circular
+          indeterminate
+        />
+
+        <v-expansion-panel>
+          <v-expansion-panel-content>
+            <div slot="header">hello</div>
+            hello world
+          </v-expansion-panel-content>
+          <v-expansion-panel-content>
+            <div slot="header">hello</div>
+            hello world
+          </v-expansion-panel-content>
+          <v-expansion-panel-content>
+            <div slot="header">hello</div>
+            hello world
+          </v-expansion-panel-content>
+        </v-expansion-panel>
+      </v-container>
+    </v-content>
+  </div>
 </template>
 
 <script>
-import HelloWorld from '~/components/HelloWorld.vue'
-
 export default {
-  components: {
-    HelloWorld
+  components: {},
+  data() {
+    return {}
   }
 }
 </script>
